157bd03be9b812037833b1780f005aeeb3b2104b
SunHuy <nghuy1115@gmail.com>
2025-05-27 01:34:33 +0700

    Remove unusable annotation in repository

--- backend/eshop/src/main/java/com/example/eshop/repository/interfaces/ProductJpaRepository.java
+++ backend/eshop/src/main/java/com/example/eshop/repository/interfaces/ProductJpaRepository.java
@@ -2,11 +2,9 @@ package com.example.eshop.repository.interfaces;
 
 import com.example.eshop.model.Product;
 import jakarta.persistence.LockModeType;
-import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -15,7 +13,6 @@ import java.util.List;
 @Repository
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({ @QueryHint(name = "javax.persistence.lock.timeout", value = "3000") }) // 3 seconds timeout
     @Query("SELECT p FROM Product p WHERE p.productId IN :ids")
     List<Product> findAllByIdForUpdate(@Param("ids") List<Long> ids);
 }
